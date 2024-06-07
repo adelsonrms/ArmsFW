1179bbe11b3ae7fad39f23946efaa5b082b980d1
ADELSON RM SILVA <adelsons@gmail.com>
2024-06-06 21:53:52 -0300

    Atualizacao de notas

--- obj/Debug/net5.0/ArmsFW.AssemblyInfo.cs
+++ obj/Debug/net5.0/ArmsFW.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCopyrightAttribute("ARMS 2023")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Versão do framework ArmsFW atualizado para Net 7.0 - v3")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("2.2023.603.1")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2023.0603.1+18abb3c6d65965db7473bcd080b03c1695019120")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("2.2023.0603.1+11e1f62bdbb3340cb1353a194509bae565f2238d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArmsFW")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArmsFW")]
 [assembly: System.Reflection.AssemblyVersionAttribute("2.2023.603.1")]
